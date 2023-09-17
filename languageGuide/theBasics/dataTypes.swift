8129735bd28ed0e91824beec2b4fa717d0207a05
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-09-17 16:12:25 -0300

    Create dataTypes.swift

---
+++ languageGuide/theBasics/dataTypes.swift
@@ -0,0 +1,46 @@
+//Tipos Numéricos
+var num: Int = -2 //Representa números inteiros, positivos e negativos, sem parte decimal.
+var idade: UInt = 16 //Representa números inteiros não negativos (sem sinal).
+var pi: Double = 3,1415926 //Representa números de ponto flutuante de precisão dupla(pelo menos 15 dígitos significativos).
+var dinheiro: Float = 21,70 //Representa números de ponto flutuante de precisão simples(pelo menos 6 dígitos significativos).
+
+//Tipos de Texto
+var saudacao: String = "Olá, seja bem vindo!" //Representa uma sequência de caracteres de texto.
+var firstLetter: Character = "T" //Representa um único caractere.
+
+//Tipos Lógicos
+var myBool = true //Representa valores booleanos, ou seja, true (verdadeiro) ou false (falso).
+
+
+--EM EDICAO -- 
+//Tipos de Coleções
+Array: Representa uma coleção ordenada de elementos do mesmo tipo.
+Set: Representa uma coleção não ordenada de elementos únicos.
+Dictionary: Representa uma coleção de pares chave-valor.
+
+//Tipos Opcionais:
+Optional: Permite representar valores que podem estar ausentes (nil).
+
+//Tipos de Função:
+() -> Void: Representa uma função que não possui argumentos e não retorna um valor.
+
+//Tipos de Estruturas:
+Struct: Permite a criação de tipos de dados personalizados, como uma estrutura.
+
+//Tipos de Classes:
+Class: Permite a criação de tipos de dados personalizados, que suportam herança.
+
+//Tipos de Enumerações:
+Enum: Permite a criação de tipos de dados que podem ter um de vários casos (valores).
+
+//Tipos de Tuplas:
+Tuple: Permite agrupar valores de tipos diferentes.
+
+//Tipos de Closures:
+Closure: Representa um bloco de código que pode ser armazenado e passado como um valor.
+
+//Tipos de Protocolos:
+Protocol: Define um conjunto de requisitos que as classes ou estruturas podem adotar.
+
+//Tipos de Índices de Coleções:
+Index: Usado para acessar elementos em uma coleção.
