2c71e5ba29d36bd2c4360dfca699f5c8298edb87
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-09-17 15:18:22 -0300

    Create typeAnnotations.swift

---
+++ languageGuide/theBasics/typeAnnotations.swift
@@ -0,0 +1,7 @@
+//You can provide a type annotation when you declare a constant or variable, to be clear about the kind of values the constant or variable can store.
+//If you provide an initial value for a constant or variable at the point that it’s defined, Swift can almost always infer the type to be used for that constant or variable.
+
+var welcomeMessage: String = "Bem-Vindo!"
+var notas: Double = 8.5
+let pi = 3.14 //Tipo definido de forma implícita
+
