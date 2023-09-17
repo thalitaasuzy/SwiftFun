ab10facb4ed263d3b3e8124254f9fc7060dbb372
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-09-17 15:08:44 -0300

    Update and rename languageGuide/theBasics.swift to languageGuide/theBasics/constants&Variables.swift

--- languageGuide/theBasics.swift
+++
@@ -1,4 +0,0 @@
-//You declare constants with the let keyword and variables with the var keyword.
-
-let nome = "Thalita"
-var idade = 16
---
+++ languageGuide/theBasics/constants&Variables.swift
@@ -0,0 +1,7 @@
+//You declare constants with the let keyword and variables with the var keyword.
+
+//Note
+//If a stored value in your code won’t change, always declare it as a constant with the let keyword. Use variables only for storing values that need to be able to change.
+
+let nome = "Thalita"
+var idade = 16
