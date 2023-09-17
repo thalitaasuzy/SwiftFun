76d5c24646b7e54d085357aefb5514d8e9c72e63
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-09-17 15:36:49 -0300

    Create stringInterpolation.swift

---
+++ languageGuide/theBasics/stringInterpolation.swift
@@ -0,0 +1,6 @@
+/*Swift uses string interpolation to include the name of a constant or variable as a placeholder in a longer string,
+and to prompt Swift to replace it with the current value of that constant or variable. */
+
+var nome = "Thalita"
+print("Olá \(nome), Bem vinda!! ")
+// Prints "Olá Thalita, Bem vinda!!"
