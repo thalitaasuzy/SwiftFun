501d7f5e76476647ad4af3907bedf6eded4c2c68
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-12-15 17:19:36 -0300

    Add files via upload

---
+++ WeatherAPI/WeatherAPI/Views/WelcomeView.swift
@@ -0,0 +1,43 @@
+//
+//  WelcomeView.swift
+//  WeatherAPI
+//
+//  Created by user on 14/10/23.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the Weather App")
+                    .bold().font(.title)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+                
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
